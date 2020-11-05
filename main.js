39dfb3c677aeb39459d3db0c828bb8f568090db6
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-05 16:06:11 -0600

    created scss file

---
+++ main.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Title</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.scss">
+</head>
+<body>
+    <p>This is a paragraph.</p>
+</body>
+<script type="module" src="main.js"></script>
+</html>
