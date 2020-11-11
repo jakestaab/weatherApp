1979165521979505a06c2331c719375e91a5c6b4
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-10 19:11:35 -0600

    setup environment

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import { WEATHER_API_KEY } from './apikey.js';
 
+const getWeather = async () => {
+    const weather = await fetch("")
+    const weatherData = await weather.json();
+    console.log(weather);
+}
+
+getWeather();
