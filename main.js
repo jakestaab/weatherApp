b3f2b5528b068c4d03eeef688b8023efdc0afd5e
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-11 15:01:36 -0600

    made basic skeleton of project

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ const getWeather = async () => {
 }
 
 getWeather();
+
+
+window.onload = function() {
+    let conditions = "Haze";
+    let temp = "85";
+    let wind = "12MPH";
+    let humidity = "80%";
+    let visibility = "10 Miles";
+    document.getElementById('conditions').innerHTML = conditions;
+    document.getElementById('temp').innerHTML = temp;
+    document.getElementById('wind').innerHTML = wind;
+    document.getElementById('humidity').innerHTML = humidity;
+    document.getElementById('visibility').innerHTML = visibility;
+};
