d7cb3a6f2ff1cc5c407af28bd9d8be2d0722a0ec
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-12 13:49:40 -0600

    got search function to work

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 import { WEATHER_API_KEY } from './apikey.js';
 
+let defaultLocation = "Lawrence,Kansas";
+let userLocation;
+
+//sets userLocation to value entered into search when search button clicked
+const locationButton = document.getElementById("locale");
+locationButton.addEventListener("click", (event) => {
+    userLocation = document.getElementById("userLocation").value;
+    const getUserWeather = async () => {
+        const data = await fetch(weatherURL(userLocation));
+        const weatherData = await data.json();
+        console.log(weatherData);
+        let conditions = weatherData.weather[0].main;
+        let temp = weatherData.main.temp;
+        let wind = weatherData.wind.speed;
+        let humidity = weatherData.main.humidity;
+        let visibility = (weatherData.visibility / 1000);
+        document.getElementById('conditions').innerHTML = conditions;
+        document.getElementById('temp').innerHTML = temp;
+        document.getElementById('wind').innerHTML = wind;
+        document.getElementById('humidity').innerHTML = humidity;
+        document.getElementById('visibility').innerHTML = visibility;
+    }
+    getUserWeather();
+}, false);
+
+
 //accepts parameter of a location
 //returns the API URL with that location
 const weatherURL = (location) => {
     //WEATHER_API_KEY is a function that returns my API key
     let key = WEATHER_API_KEY();
     return "http://api.openweathermap.org/data/2.5/weather?q=" +
-                     location + "&appid=" + key;
+                     location + "&units=imperial&appid=" + key;
 }
 
-const weatherList = {
-    list: []
-}
-
+//retrieves the weather API data as JSON object
+//inputs object property values into HTML
 const getWeather = async () => {
-    const data = await fetch(weatherURL("Lawrence,Kansas"));
+    const data = await fetch(weatherURL(defaultLocation));
     const weatherData = await data.json();
     console.log(weatherData);
     let conditions = weatherData.weather[0].main;
@@ -28,5 +52,4 @@ const getWeather = async () => {
     document.getElementById('humidity').innerHTML = humidity;
     document.getElementById('visibility').innerHTML = visibility;
 }
-
 getWeather();
