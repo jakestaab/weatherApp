f62debfc51e8fcb745d68156978e31a355766d73
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-11 19:11:37 -0600

    API fetch worked out

--- main.js
+++ main.js
@@ -2,31 +2,30 @@ import { WEATHER_API_KEY } from './apikey.js';
 
 //accepts parameter of a location
 //returns the API URL with that location
-function weatherURL(location) {
+const weatherURL = (location) => {
     //WEATHER_API_KEY is a function that returns my API key
     let key = WEATHER_API_KEY();
     return "http://api.openweathermap.org/data/2.5/weather?q=" +
                      location + "&appid=" + key;
 }
 
-
-const getWeather = async () => {
-    const weather = await fetch(weatherURL("Lawrence,Kansas"));
-    const weatherData = await weather.json();
-    console.log(weatherData);
+const weatherList = {
+    list: []
 }
 
-getWeather();
-
-window.onload = function() {
+const getWeather = async () => {
+    const data = await fetch(weatherURL("Lawrence,Kansas"));
+    const weatherData = await data.json();
     let conditions = "Haze";
-    let temp = "85";
-    let wind = "12MPH";
-    let humidity = "80%";
-    let visibility = "10 Miles";
+    let temp = weatherData.main.temp;
+    let wind = weatherData.wind.speed;
+    let humidity = weatherData.main.humidity;
+    let visibility = (weatherData.visibility / 1000);
     document.getElementById('conditions').innerHTML = conditions;
     document.getElementById('temp').innerHTML = temp;
     document.getElementById('wind').innerHTML = wind;
     document.getElementById('humidity').innerHTML = humidity;
     document.getElementById('visibility').innerHTML = visibility;
-};
+}
+
+getWeather();
