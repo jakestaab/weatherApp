093c473016439a6631f4bd1f8fa31ce7325a2f99
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-13 17:46:24 -0600

    added icon

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { WEATHER_API_KEY } from './apikey.js';
 import { windConversion, capitalizeLocation } from './format_funcs.js';
-//import { capitalizeLocation } from './format_funcs.js';
 
 let defaultLocation;
 
@@ -35,10 +34,16 @@ setLocationButton.addEventListener("click", (event) => {
     localStorage.setItem("userSetLocation", JSON.stringify(defaultLocation));
 })
 
+const iconURL = (code) => {
+    let insert = String(code);
+    return "http://openweathermap.org/img/wn/" + insert + "@2x.png";
+}
+
 //retrieves weather API data and inputs object property values into HTML
 const getWeather = async () => {
     const data = await fetch(weatherURL(defaultLocation));
     const weatherData = await data.json();
+    let icon = iconURL(weatherData.weather[0].icon);
     let conditions = weatherData.weather[0].main;
     let temp = weatherData.main.temp;
     let wind = weatherData.wind.speed;
@@ -46,9 +51,13 @@ const getWeather = async () => {
     let visibility = (weatherData.visibility / 1000);
     let pressure = (weatherData.main.pressure / 33.86);
     let direction = weatherData.wind.deg;
+
+    console.log(icon);
+
+    document.getElementById('icon').src = icon;
     document.getElementById('conditions').innerHTML = conditions;
-    document.getElementById('temp').innerHTML = temp;
-    document.getElementById('wind').innerHTML = Math.round(wind);
+    document.getElementById('temp').innerHTML = Math.round(temp) + "°";
+    document.getElementById('wind').innerHTML = Math.round(wind) + "mph";
     document.getElementById('humidity').innerHTML = humidity;
     document.getElementById('visibility').innerHTML = visibility;
     document.getElementById('pressure').innerHTML = pressure.toFixed(2);
