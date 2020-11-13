9e18cde77e482074707ef05c83306780e4124ecf
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-13 09:47:47 -0600

    got persistent data working correctly

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 import { WEATHER_API_KEY } from './apikey.js';
 
-let defaultLocation = "Lawrence,Kansas";
+let defaultLocation;
 let userLocation;
+
+//sets default location in local storage when user clicks setlocation button
+const setLocationButton = document.getElementById("setlocation");
+setLocationButton.addEventListener("click", (event) => {
+    localStorage.setItem("userSetLocation", JSON.stringify(userLocation));
+})
+
+//checks if user has set a default location manually
+//if so, sets defaultLocation to that location, otherwise, defaults to Lawrence, Kansas
+function locationCheck() {
+    if(localStorage.getItem("userSetLocation")) {
+        defaultLocation = JSON.parse(localStorage.getItem("userSetLocation"));
+    } else {
+        defaultLocation = "Lawrence, Kansas";
+    }
+}
+locationCheck();
+
+//accepts parameter of a location
+//returns the API URL with that location
+const weatherURL = (location) => {
+    //WEATHER_API_KEY is a function that returns my API key
+    let key = WEATHER_API_KEY();
+    return "http://api.openweathermap.org/data/2.5/weather?q=" +
+                     location + "&units=imperial&appid=" + key;
+}
 
 //sets userLocation to value entered into search when search button clicked
 const locationButton = document.getElementById("locale");
@@ -26,21 +52,6 @@ locationButton.addEventListener("click", (event) => {
     getUserWeather();
 }, false);
 
-const setLocationButton = document.getElementById("setlocation");
-setLocationButton.addEventListener("click", (event) => {
-    localStorage.setItem("userSetLocation", JSON.stringify(userLocation));
-    locationSet = true;
-})
-
-//accepts parameter of a location
-//returns the API URL with that location
-const weatherURL = (location) => {
-    //WEATHER_API_KEY is a function that returns my API key
-    let key = WEATHER_API_KEY();
-    return "http://api.openweathermap.org/data/2.5/weather?q=" +
-                     location + "&units=imperial&appid=" + key;
-}
-
 //retrieves the weather API data as JSON object
 //inputs object property values into HTML
 const getWeather = async () => {
@@ -57,5 +68,7 @@ const getWeather = async () => {
     document.getElementById('wind').innerHTML = wind;
     document.getElementById('humidity').innerHTML = humidity;
     document.getElementById('visibility').innerHTML = visibility;
+    document.getElementById('shownlocation').innerHTML = defaultLocation;
 }
 getWeather();
+
