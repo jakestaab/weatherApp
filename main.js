0c875c7444a66891dbaa89834fa54ae6a213ea7c
Jake Staab <jjstaab@mail.fhsu.edu>
2020-11-23 17:18:06 -0600

    de-nested async functions

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { windConversion, capitalizeLocation } from './format_funcs.js';
 
 let defaultLocation;
 let key = "e6829fea390bfd66e1381953b9327c55"; //openweathermap.org API key
-
 const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 function getDay(offset = 0) {
     return week[(new Date().getDay() + offset) % week.length]
@@ -22,9 +21,22 @@ const setLocationButton = document.getElementById("setlocation");
     localStorage.setItem("userSetLocation", JSON.stringify(defaultLocation));
 })
 
-function showLocation(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+//accepts parameter of a location; returns the API URL with that location
+const weatherURL = (location) => {
+    return "https://api.openweathermap.org/data/2.5/weather?q=" +
+                    location + "&units=imperial&appid=" + key;
+}
+
+//accepts parameter of API icon code; returns API URL for that icon code
+const iconURL = (code) => {
+    return "https://openweathermap.org/img/wn/" + String(code) + "@2x.png";
+}
+
+//accepts lat and long coords, returns API URL for daily weather at that location
+const dailyURL = (lat, lon) => {
+    return "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat + "&lon=" + lon + "&exclude=currently,minutely,alerts" +
+            "&units=imperial&appid=" + key;
 }
 
 navigator.geolocation.getCurrentPosition((position) => {
@@ -39,35 +51,62 @@ navigator.geolocation.getCurrentPosition((position) => {
       }
   }
 
+//fetches weather API data and inputs object property values into HTML
+const getWeather = async () => {
+    //today
+    const data = await fetch(weatherURL(defaultLocation));
+    const weatherData = await data.json();
+
+    //five-day
+    const daily = await fetch(dailyURL(weatherData.coord.lat, weatherData.coord.lon));
+    const dailyData = await daily.json();
+
+    //today
+    let icon = iconURL(weatherData.weather[0].icon);
+    let feelslike = (weatherData.main.feels_like);
+    let conditions = weatherData.weather[0].main;
+    let temp = weatherData.main.temp;
+    let todayhigh = weatherData.main.temp_max;
+    let todaylow = weatherData.main.temp_min;
+    let wind = weatherData.wind.speed;
+    let direction = weatherData.wind.deg;
+    let humidity = weatherData.main.humidity;
+    let pressure = (weatherData.main.pressure / 33.86);
+    document.getElementById('icon').src = icon;
+    document.getElementById('feelslike').innerHTML = Math.round(feelslike) + "°";
+    document.getElementById('conditions').innerHTML = conditions;
+    document.getElementById('temp').innerHTML = Math.round(temp) + "°";
+    document.getElementById('todayhigh').innerHTML = Math.round(todayhigh) + "°";
+    document.getElementById('todaylow').innerHTML = Math.round(todaylow) + "°";
+    document.getElementById('wind').innerHTML = Math.round(wind);
+    document.getElementById('direction').innerHTML = windConversion(direction);
+    document.getElementById('humidity').innerHTML = humidity + "%";
+    document.getElementById('pressure').innerHTML = pressure.toFixed(2);
+    document.getElementById('shownlocation').innerHTML = capitalizeLocation(defaultLocation);
+
+    //five-day
+    [1, 2, 3, 4, 5].forEach(day => {
+        document.getElementById(`icon${day}`).src = iconURL(dailyData.daily[day].weather[0].icon);
+        document.getElementById(`sky${day}`).innerHTML = dailyData.daily[day].weather[0].main;
+
+        document.getElementById(`day${day}`).innerHTML = getDay(day - 1);
+
+        const high = Math.round(dailyData.daily[day].temp.max);
+        document.getElementById(`high${day}`).innerHTML = `${high}°`;
+
+        const low = Math.round(dailyData.daily[day].temp.min);
+        document.getElementById(`low${day}`).innerHTML = `${low}°`;
+    })
+}
+
 const getURL = async (lat, lon) => {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showLocation, errorHandler);
-    }
-    
-    //uses geolocation for lat/lon, then calls API to get city name for those coords
-    const APILocationURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat +
-    "&lon=" + lon + "&appid=" + key;
-    const APIData = await fetch(APILocationURL);
-    const APIDataParsed = await APIData.json();
-    autoLocation = APIDataParsed.name;
-    //localStorage.setItem("geoLocale", JSON.stringify(APIDataParsed.name));
-
-    //accepts parameter of a location; returns the API URL with that location
-    const weatherURL = (location) => {
-        return "https://api.openweathermap.org/data/2.5/weather?q=" +
-                        location + "&units=imperial&appid=" + key;
-    }
-
-    //accepts parameter of API icon code; returns API URL for that icon code
-    const iconURL = (code) => {
-        return "https://openweathermap.org/img/wn/" + String(code) + "@2x.png";
-    }
-
-    //accepts lat and long coords, returns API URL for daily weather at that location
-    const dailyURL = (lat, lon) => {
-        return "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-                lat + "&lon=" + lon + "&exclude=currently,minutely,alerts" +
-                "&units=imperial&appid=" + key;
+    if (lat != null && lon != null) {
+        const APILocationURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat +
+        "&lon=" + lon + "&appid=" + key;
+        const APIData = await fetch(APILocationURL);
+        const APIDataParsed = await APIData.json();
+        autoLocation = APIDataParsed.name;
+        localStorage.setItem("geoLocale", JSON.stringify(APIDataParsed.name));
     }
 
     //assigns to defaultLocation the city data to be displayed on page.
@@ -75,68 +114,19 @@ const getURL = async (lat, lon) => {
     //then checks localStorage for geo-located location;
     //else uses "Lawrence, Kansas" as default.
     //otherwise uses what has just been searched for
-    const assignLocation = () => {
-        if(localStorage.getItem("userSetLocation")) {
-            defaultLocation = JSON.parse(localStorage.getItem("userSetLocation"));
-        } else if(autoLocation) {
-            defaultLocation = autoLocation;
-        } else {
-            defaultLocation = "Lawrence, Kansas";
-        }
+    if(localStorage.getItem("userSetLocation")) {
+        defaultLocation = JSON.parse(localStorage.getItem("userSetLocation"));
+    } else if(localStorage.getItem("geoLocale")) {
+        defaultLocation = autoLocation;
+    } else {
+        defaultLocation = "Lawrence, Kansas";
     }
-    assignLocation();
 
-
-
-
-    //fetches weather API data and inputs object property values into HTML
-    const getWeather = async () => {
-        //today
-        const data = await fetch(weatherURL(defaultLocation));
-        const weatherData = await data.json();
-
-        //five-day
-        const daily = await fetch(dailyURL(weatherData.coord.lat, weatherData.coord.lon));
-        const dailyData = await daily.json();
-        console.log(daily);
-
-        //today
-        let icon = iconURL(weatherData.weather[0].icon);
-        let feelslike = (weatherData.main.feels_like);
-        let conditions = weatherData.weather[0].main;
-        let temp = weatherData.main.temp;
-        let todayhigh = weatherData.main.temp_max;
-        let todaylow = weatherData.main.temp_min;
-        let wind = weatherData.wind.speed;
-        let direction = weatherData.wind.deg;
-        let humidity = weatherData.main.humidity;
-        let pressure = (weatherData.main.pressure / 33.86);
-        document.getElementById('icon').src = icon;
-        document.getElementById('feelslike').innerHTML = Math.round(feelslike) + "°";
-        document.getElementById('conditions').innerHTML = conditions;
-        document.getElementById('temp').innerHTML = Math.round(temp) + "°";
-        document.getElementById('todayhigh').innerHTML = Math.round(todayhigh) + "°";
-        document.getElementById('todaylow').innerHTML = Math.round(todaylow) + "°";
-        document.getElementById('wind').innerHTML = Math.round(wind);
-        document.getElementById('direction').innerHTML = windConversion(direction);
-        document.getElementById('humidity').innerHTML = humidity + "%";
-        document.getElementById('pressure').innerHTML = pressure.toFixed(2);
-        document.getElementById('shownlocation').innerHTML = capitalizeLocation(defaultLocation);
-
-        //five-day
-        [1, 2, 3, 4, 5].forEach(day => {
-            document.getElementById(`icon${day}`).src = iconURL(dailyData.daily[day].weather[0].icon);
-            document.getElementById(`sky${day}`).innerHTML = dailyData.daily[day].weather[0].main;
-    
-            document.getElementById(`day${day}`).innerHTML = getDay(day - 1);
-    
-            const high = Math.round(dailyData.daily[day].temp.max);
-            document.getElementById(`high${day}`).innerHTML = `${high}°`;
-    
-            const low = Math.round(dailyData.daily[day].temp.min);
-            document.getElementById(`low${day}`).innerHTML = `${low}°`;
-        })
-    }
-    getWeather();
+    await getWeather();
 }
 
+navigator.geolocation.getCurrentPosition(position => {
+    getURL(position.coords.latitude, position.coords.longitude);
+}, () => {
+    getURL();
+});
